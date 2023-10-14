10a2e9d599ff375940ca2c67713deaa1ba95d51d
“Ball-Zilla” <papaabeku.aom@gmail.comgit config --global user.name Ball-Zillagit config --global user.email papaabeku.aom@gmail.com>
2023-10-14 16:30:57 -0700

    Another One

--- builtins_env.c
+++ builtins_env.c
@@ -1,48 +1,61 @@
-                errno = 127;
-        }
-        return (0);
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include "your_custom_header.h"  // Include your custom header file if needed
+
+typedef struct {
+    char **tokens;
+    char *command_name;
+} data_of_program;
 
 /**
- * builtin_set_env - ..
+ * builtin_set_env - Set an environment variable.
  * @data: struct for the program's data
- * Return: zero if sucess, or other number if its declared in the arguments
+ * Return: Zero if success, or an error number if an issue occurs.
  */
-int builtin_set_env(data_of_program *data)
-{
-        /* validate args */
-        if (data->tokens[1] == NULL || data->tokens[2] == NULL)
-                return (0);
-        if (data->tokens[3] != NULL)
-        {
-                errno = E2BIG;
-                perror(data->command_name);
-                return (5);
-        }
+int builtin_set_env(data_of_program *data) {
+    // Validate args
+    if (data->tokens[1] == NULL || data->tokens[2] == NULL) {
+        return 0;
+    }
 
-        env_set_key(data->tokens[1], data->tokens[2], data);
+    if (data->tokens[3] != NULL) {
+        errno = E2BIG;
+        perror(data->command_name);
+        return 5;
+    }
 
-        return (0);
+    env_set_key(data->tokens[1], data->tokens[2], data);
+
+    return 0;
 }
 
 /**
- * builtin_unset_env - ..
- * @data: struct for the program's data'
- * Return: ..
+ * builtin_unset_env - Unset an environment variable.
+ * @data: struct for the program's data
+ * Return: Zero if success, or an error number if an issue occurs.
  */
-int builtin_unset_env(data_of_program *data)
-{
-        /* validate args */
-        if (data->tokens[1] == NULL)
-                return (0);
-        if (data->tokens[2] != NULL)
-        {
-                errno = E2BIG;
-                perror(data->command_name);
-                return (5);
-        }
-        env_remove_key(data->tokens[1], data);
+int builtin_unset_env(data_of_program *data) {
+    // Validate args
+    if (data->tokens[1] == NULL) {
+        return 0;
+    }
 
-        return (0);
+    if (data->tokens[2] != NULL) {
+        errno = E2BIG;
+        perror(data->command_name);
+        return 5;
+    }
+
+    env_remove_key(data->tokens[1], data);
+
+    return 0;
 }
-"builtins.c" 92L, 2015C                                                                                                                     92,1          Bot
+
+int main() {
+    // Your main program logic here
+
+    return 0;
+}
+
