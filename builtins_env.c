8b27410a8b12098744581ed88c2274bb52a7fc1a
Eael <teyeearl@gmail.com>
2023-10-18 11:08:22 +0000

    Checking Betty 1

--- builtins_env.c
+++ builtins_env.c
@@ -9,25 +9,32 @@
  * @data: struct for the program's data
  * Return: Zero if success, or an error number if an issue occurs.
  */
-int builtin_env(data_of_program *data){
+int builtin_env(data_of_program *data)
+{
 	int i;
 	char cpname[50] = {'\0'};
 	char *var_copy = NULL;
 
 	if (data->tokens[1] == NULL)
 		print_environ(data);
-	else {
-		for (i = 0; data->tokens[1][i]; i++) {
-			if (data->tokens[1][i] == '=') {
+	else
+	{
+		for (i = 0; data->tokens[1][i]; i++)
+		{
+			if (data->tokens[1][i] == '=')
+			{
 				var_copy = str_duplicate(env_get_key(cpname, data));
-				if (var_copy != NULL) {
+				if (var_copy != NULL)
+				{
 					env_set_key(cpname, data->tokens[1] + i + 1, data);
 					print_environ(data);
-					if (env_get_key(cpname, data) == NULL) {
+					if (env_get_key(cpname, data) == NULL)
+					{
 						_print(data->tokens[1]);
 						_print("\n");
 					}
-					else {
+					else
+					{
 						env_set_key(cpname, var_copy, data);
 						free(var_copy);
 					}
@@ -49,20 +56,21 @@ int builtin_env(data_of_program *data){
  * @data: struct for the program's data
  * Return: Zero if success, or an error number if an issue occurs.
  */
-int builtin_set_env(data_of_program *data) {
-	if (data->tokens[1] == NULL || data->tokens[2] == NULL) {
-		return 0;
-	}
+int builtin_set_env(data_of_program *data)
+{
+	if (data->tokens[1] == NULL || data->tokens[2] == NULL)
+		return (0);
 
-	if (data->tokens[3] != NULL) {
+	if (data->tokens[3] != NULL)
+	{
 		errno = E2BIG;
 		perror(data->command_name);
-		return 5;
+		return (5);
 	}
 
 	env_set_key(data->tokens[1], data->tokens[2], data);
 
-	return 0;
+	return (0);
 }
 
 /**
@@ -70,18 +78,19 @@ int builtin_set_env(data_of_program *data) {
  * @data: struct for the program's data
  * Return: Zero if success, or an error number if an issue occurs.
  */
-int builtin_unset_env(data_of_program *data) {
-	if (data->tokens[1] == NULL) {
-		return 0;
-	}
+int builtin_unset_env(data_of_program *data)
+{
+	if (data->tokens[1] == NULL)
+		return (0);
 
-	if (data->tokens[2] != NULL) {
+	if (data->tokens[2] != NULL)
+	{
 		errno = E2BIG;
 		perror(data->command_name);
-		return 5;
+		return (5);
 	}
 
 	env_remove_key(data->tokens[1], data);
 
-	return 0;
+	return (0);
 }
