adfaad4eff5f419266a8825893378ebacbe86a1a
Eael <teyeearl@gmail.com>
2023-10-18 11:02:05 +0000

    Checking Betty

--- builtins_env.c
+++ builtins_env.c
@@ -5,7 +5,7 @@
 #include "shell.h"
 
 /**
- * builtin_env - shows the environment where the shell runs 
+ * builtin_env - shows the environment where the shell runs
  * @data: struct for the program's data
  * Return: Zero if success, or an error number if an issue occurs.
  */
@@ -50,19 +50,19 @@ int builtin_env(data_of_program *data){
  * Return: Zero if success, or an error number if an issue occurs.
  */
 int builtin_set_env(data_of_program *data) {
-    if (data->tokens[1] == NULL || data->tokens[2] == NULL) {
-        return 0;
-    }
+	if (data->tokens[1] == NULL || data->tokens[2] == NULL) {
+		return 0;
+	}
 
-    if (data->tokens[3] != NULL) {
-        errno = E2BIG;
-        perror(data->command_name);
-        return 5;
-    }
+	if (data->tokens[3] != NULL) {
+		errno = E2BIG;
+		perror(data->command_name);
+		return 5;
+	}
 
-    env_set_key(data->tokens[1], data->tokens[2], data);
+	env_set_key(data->tokens[1], data->tokens[2], data);
 
-    return 0;
+	return 0;
 }
 
 /**
@@ -71,17 +71,17 @@ int builtin_set_env(data_of_program *data) {
  * Return: Zero if success, or an error number if an issue occurs.
  */
 int builtin_unset_env(data_of_program *data) {
-    if (data->tokens[1] == NULL) {
-        return 0;
-    }
+	if (data->tokens[1] == NULL) {
+		return 0;
+	}
 
-    if (data->tokens[2] != NULL) {
-        errno = E2BIG;
-        perror(data->command_name);
-        return 5;
-    }
+	if (data->tokens[2] != NULL) {
+		errno = E2BIG;
+		perror(data->command_name);
+		return 5;
+	}
 
-    env_remove_key(data->tokens[1], data);
+	env_remove_key(data->tokens[1], data);
 
-    return 0;
+	return 0;
 }
